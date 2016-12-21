bae72e1318118c18d4846a930224d5b3167bcf15
phermes <phermes@cern.ch>
2016-12-21 15:27:05 +0100

    initial commit

---
+++ run.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#### PHFIGO
+#    PH FINANCE ALGO - MAIN PROGRAM
+
+#### PROGRAM INFORMATION
+
+__version__  = '0.0.2dev'
+version_date = '21.12.2016'
+author       = 'pdh'
+
+#### SWITCHES AND KNOBS
+
+debug      = True                                                 # switch on/off the debugging mode
+Filter     = True                                                 # filter the stocks following the selection
+
+#### LOAD DEPENDENCIES
+
+
+import datetime
+
